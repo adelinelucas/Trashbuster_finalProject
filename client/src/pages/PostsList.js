abe99256e0556a88a2c47f7ee52e697cc1c5f563
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-05-18 20:46:26 +0200

    reload data liste_des_actions pages

--- client/src/pages/PostsList.js
+++ client/src/pages/PostsList.js
@@ -1,28 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Posts from '../components/Posts';
 import Loading from '../components/Loading';
 import { useGlobalContext } from '../app/context';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useSearchParams } from 'react-router-dom';
 
 const PostsList = () => {
     const {posts, loading,actionsNumber, fetchActionsNumber, fetchPosts} = useGlobalContext();
-
+    const [messageBD, setMessageDB] = useState(false)
+    const [chargeData, setChargeDatas] = useState(false);
     useEffect( ()=>{
         fetchPosts();
         fetchActionsNumber();
     },[]);
+    useEffect( ()=>{
+        fetchPosts();
+        fetchActionsNumber();
+    },[chargeData]);
+    useEffect(()=>{
+        let timer = setTimeout(()=>{
+            setMessageDB(true)
+        }, 2000)
+        return ()=> clearTimeout(timer)
+    }, [])
 
     if(loading){
         return (
             <section className="w-full flex flex-col items-center min-h-screen my-4">
                 <Loading/>
-                <aside className='text-center w-full'>
-                    <p className='text-brightYellow'>Si le chargement des données ne se fait pas, voici une vue avec de pages :</p>
-                    <button className="m-2 p-2 bg-orangeV2 text-white cursor-pointer btnNavBar btnNavBarShadow">
-                    <NavLink to={'/liste_des_actions_v2'}>Voir les actions
-                    </NavLink>
-                </button>
-                </aside>
+                {messageBD && 
+                    <aside className='text-center w-full'>
+                        <p className='text-brightYellow'>Si le chargement des données ne se fait pas, voici une vue avec de pages :</p>
+                        <button className="m-2 p-2 bg-orangeV2 text-white cursor-pointer btnNavBar btnNavBarShadow">
+                            <NavLink to={'/liste_des_actions_v2'}>Voir les actions
+                        </NavLink>
+                        </button>
+                        <button className="m-2 p-2 bg-brightYellow text-white cursor-pointer btnNavBar btnNavBarShadow" type='button' onClick={()=>setChargeDatas(!chargeData)} >Recharger les données
+                        </button>
+                    </aside>
+                }
                 
             </section>
         )
